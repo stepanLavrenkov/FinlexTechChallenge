cf4053bcab1904911822b9e0d2143866c9e70446
Stepan <stepan@Stepans-MacBook-Pro.local>
2022-10-08 20:23:56 +0200

    Cypress + cucumber setup

---
+++ cypress.config.ts
@@ -0,0 +1,46 @@
+import fs from 'fs-extra';
+import path from 'path';
+import _ from 'lodash';
+import { defineConfig } from 'cypress';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+
+export default defineConfig({
+  e2e: {
+    specPattern: '**/*.feature',
+    supportFile: false,
+    async setupNodeEvents(on, config) {
+      const configName = config.env.configFile || 'production';
+      const fileConfig = await getConfigurationFromFile(configName);
+      config.video = false;
+
+      _.merge(config, fileConfig);
+
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)]
+        })
+      );
+
+      return config;
+    }
+  }
+});
+
+async function getConfigurationFromFile(
+  fileName: string
+): Promise<Cypress.PluginConfigOptions> {
+  const pathToConfigFile = path.resolve(
+    'cypress',
+    'config',
+    `${fileName}.json`
+  );
+
+  return fs.readJSONSync(
+    pathToConfigFile
+  ) as unknown as Cypress.PluginConfigOptions;
+}
